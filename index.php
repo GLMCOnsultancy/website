dd1c1d960873a71b1300587a1af7d232af65ca97
mintudutta <mintu.cs04@gmail.com>
2021-07-21 17:26:11 +0530

    Intial version

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+//print_r($_SERVER);die();
+
+$sitepad['db_name'] = 'qufuzpjr_spad429';
+$sitepad['db_user'] = 'qufuzpjr_spad429';
+$sitepad['db_pass'] = 'YMr1.Z(W.M';
+$sitepad['db_host'] = 'localhost';
+$sitepad['db_table_prefix'] = 'iHUh0aO_';
+$sitepad['charset'] = 'utf8mb4';
+$sitepad['collate'] = 'utf8mb4_unicode_ci';
+$sitepad['serving_url'] = 'greatmindslab.com';// URL without protocol but with directory as well
+$sitepad['url'] = 'https://greatmindslab.com';
+$sitepad['relativeurl'] = '';
+$sitepad['.sitepad'] = '/home/qufuzpjr';
+$sitepad['sitepad_plugin_path'] = '/usr/local/sitepad';
+$sitepad['editor_path'] = '/usr/local/sitepad/editor';
+$sitepad['path'] = dirname(__FILE__);
+$sitepad['AUTH_KEY'] = 'EXzCfEO5ukmdQ6Syp9JmJjoGL70cVZKt8VhlAsCiYyYXupfFtyS42A9hCUeoxJme';
+$sitepad['SECURE_AUTH_KEY'] = 'NarAW1SV1bwQSRebemqsVFlhpXgvbIIextiOHUf3lfRid0rcQXMTXzeEYfSvvyIF';
+$sitepad['LOGGED_IN_KEY'] = 'KMWiZWDIJAFio1Myi0FCzoysEi0ulWtBWIOQzpo7QQ4qZGATqncHyDBRmcm58fD7';
+$sitepad['NONCE_KEY'] = '0PGoWyeMupfDNaGJs7SYuJW6vNqIHzZW3fcdzsyeS6RH0ZsgAUDee1hvEF4gkPhc';
+$sitepad['AUTH_SALT'] = 'V3OgcX5Z7J5tO9mTGEf1xzhKgPfaj2RhPLGQ4nBXyhHmhHxlhUaqmImNZzBOMnNy';
+$sitepad['SECURE_AUTH_SALT'] = 'NDP01XDz33ReTReCcyisNvm6l76mz3HST9ULHlACc7xyASLKoNbJ48uIf0kU1nHh';
+$sitepad['LOGGED_IN_SALT'] = 'N744fuig2ugLfa14XkIvVKFRahEZgVIAbdIqKkm9BU45Xy7cdvAzKTqyfvVM0AhP';
+$sitepad['NONCE_SALT'] = 'ZFwFiMhj8Gh0P5MzA83Zr8Prg2Zc5KVNVHNOr7VqF0jQkhg6OVjsg6zG69J4KDmE';
+
+if(!include_once($sitepad['editor_path'].'/site-inc/bootstrap.php')){
+	die('Could not include the bootstrap.php. One of the reasons could be open_basedir restriction !');
+}
+
